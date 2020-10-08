917d59a34c6d5fea39c1c99774741673e455d411
Aymeric Mortemousque <aymeric@mortemousque.com>
2020-10-08 14:09:00 +0200

    Manage gap and relative positionning

--- 3d/3dMondrian.js
+++ 3d/3dMondrian.js
@@ -1,109 +1,116 @@
 // Find the latest version by visiting https://unpkg.com/three. The URL will
 // redirect to the newest stable release.
 import * as THREE from "https://unpkg.com/three@0.121.1/build/three.module.js";
-
-document.addEventListener("mousedown", onDocumentMouseDown, false);
-
-var projector = new THREE.Projector();
-
-function onDocumentMouseDown(event) {
-  event.preventDefault();
-
-  var vector = new THREE.Vector3(
-    (event.clientX / window.innerWidth) * 2 - 1,
-    -(event.clientY / window.innerHeight) * 2 + 1,
-    0.5
-  );
-  projector.unprojectVector(vector, camera);
-
-  var ray = new THREE.Ray(camera.position, vector.subSelf(camera.position).normalize());
-
-  var intersects = ray.intersectObjects(objects);
-
-  if (intersects.length > 0) {
-    intersects[0].object.materials[0].color.setHex(Math.random() * 0xffffff);
-
-    var particle = new THREE.Particle(particleMaterial);
-    particle.position = intersects[0].point;
-    particle.scale.x = particle.scale.y = 8;
-    scene.add(particle);
-  }
-}
+import { OrbitControls } from "https://unpkg.com/three@0.121.1/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
 document.body.appendChild(renderer.domElement);
 
-const shift = 1;
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const mondrian = new THREE.Group();
+
 const depth = 0;
-const gutter = 0.6;
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(0.9, 1.7, depth),
-  new THREE.MeshBasicMaterial({ color: 0x0384fc })
-);
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1.9, 1.7, depth),
-  new THREE.MeshBasicMaterial({ color: 0xffffff })
-);
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1.9, 0.8, depth),
-  new THREE.MeshBasicMaterial({ color: 0xeefe00 })
-);
-const cube4 = new THREE.Mesh(
-  new THREE.BoxGeometry(1.9, 0.8, depth),
-  new THREE.MeshBasicMaterial({ color: 0xffffff })
-);
-const cube5 = new THREE.Mesh(
-  new THREE.BoxGeometry(1.9, 3.5, depth),
-  new THREE.MeshBasicMaterial({ color: 0xffffff })
-);
-
-const cube6 = new THREE.Mesh(
-  new THREE.BoxGeometry(0.9, 1.7, depth),
-  new THREE.MeshBasicMaterial({ color: 0xffffff })
-);
-const cube7 = new THREE.Mesh(
-  new THREE.BoxGeometry(3.9, 1.7, depth),
-  new THREE.MeshBasicMaterial({ color: 0xff0604 })
-);
-
-scene.add(cube1);
-scene.add(cube2);
-scene.add(cube3);
-scene.add(cube4);
-scene.add(cube5);
-scene.add(cube6);
-scene.add(cube7);
-
-cube1.position.x = 0 - shift;
-cube2.position.x = cube1.position.x + 0.9 + gutter;
-cube3.position.x = cube2.position.x + 1.4 + gutter;
-cube3.position.y -= 0.45;
-
-cube4.position.x = cube2.position.x + 1.4 + gutter;
-cube4.position.y = 0.45;
-
-cube5.position.x = cube4.position.x + 1.9 + 0.1;
-cube5.position.y = -0.9;
-
-cube6.position.x = 0 - shift;
-cube6.position.y = -1.8;
-
-cube7.position.x = cube1.position.x + 1.9 + gutter;
-cube7.position.y = -1.8;
-
+const gap = 0.1;
 camera.position.z = 5;
+
+const convertUnitToDimension = (uW, uH) => {
+  const unitW = 0.9;
+  const unitH = 0.8;
+
+  return [unitW * uW + (uW - 1) * gap, unitH * uH + (uH - 1) * gap];
+};
+
+const createCube = (name, uW, uH, color) => {
+  let [width, height] = convertUnitToDimension(uW, uH);
+  let cube = new THREE.Mesh(
+    new THREE.BoxGeometry(width, height, depth),
+    new THREE.MeshBasicMaterial({ color: color })
+  );
+  cube.name = name;
+  return cube;
+};
+
+const positions = new Map();
+positions.set(createCube("cube1", 1, 2, 0x0384fc), { x: 0, y: 0 });
+positions.set(createCube("cube2", 2, 2, 0xffffff), { x: "cube1" });
+positions.set(createCube("cube3", 2, 1, 0xeefe00), { x: "cube2", y: 0 });
+positions.set(createCube("cube4", 2, 1, 0xffffff), { x: "cube2", y: "cube3" });
+positions.set(createCube("cube5", 2, 4, 0xffffff), { x: "cube3", y: 0 });
+positions.set(createCube("cube6", 1, 2, 0xffffff), { x: 0, y: "cube1" });
+positions.set(createCube("cube7", 4, 2, 0xff0604), { x: "cube6", y: "cube1" });
+
+positions.set(createCube("cube8", 1, 4, 0xffffff), { x: 0, y: "cube6" });
+positions.set(createCube("huge-white", 4, 4, 0xffffff), { x: "cube8", y: "cube6" }); // grand blanc
+positions.set(createCube("cube10", 2, 2, 0x0384fc), { x: "huge-white", y: "cube6" });
+positions.set(createCube("cube11", 2, 2, 0xffffff), { x: "huge-white", y: "cube10" });
+
+positions.set(createCube("small-blue", 1, 1, 0x0384fc), { x: 0, y: "cube11" });
+positions.set(createCube("small-white", 2, 1, 0xffffff), { x: "small-blue", y: "cube11" });
+positions.set(createCube("big-yellow", 4, 2, 0xeefe00), { x: "small-white", y: "cube11" });
+positions.set(createCube("medium-white", 3, 1, 0xffffff), { x: 0, y: "small-blue" });
+
+scene.add(mondrian);
+scene.updateMatrixWorld(true);
+
+for (let [cube] of positions) {
+  mondrian.add(cube);
+}
+
+const setPosition = (cube, position) => {
+  let x = parseInt(position.x);
+  let y = parseInt(position.y);
+  const xRef = isNaN(x) ? mondrian.getObjectByName(position.x) : null;
+  const yRef = isNaN(y) ? mondrian.getObjectByName(position.y) : null;
+  const { width, height } = cube.geometry.parameters;
+  if (xRef)
+    cube.position.x = xRef.position.x + xRef.geometry.parameters.width / 2 + width / 2 + gap;
+  else cube.position.x = (position.x ?? 0) + width / 2;
+
+  if (yRef)
+    cube.position.y = -(
+      Math.abs(yRef.position.y) +
+      yRef.geometry.parameters.height / 2 +
+      height / 2 +
+      gap
+    );
+  else cube.position.y = (position.y ?? 0) - height / 2;
+};
+// const calculatePosition = (cube, position) => {
+//   // vector.copy(cube.position);
+//   // cube.parent.worldToLocal(vector);
+//   // console.log(vector);
+//   //  position.setFromMatrixPosition(scene.getObjectByName("cube1").matrixWorld);
+
+//   // alert(position.x + "," + position.y + "," + position.z);
+
+//   if (cube.position.x < position.x && position.x > 0) cube.position.x += 0.03;
+//   if (cube.position.x > position.x && position.x < 0) cube.position.x -= 0.03;
+
+//   if (cube.position.y < position.y && position.y > 0) cube.position.y += 0.03;
+//   if (cube.position.y > position.y && position.y < 0) cube.position.y -= 0.03;
+// };
+for (let [cube, position] of positions) {
+  setPosition(cube, position);
+}
+
+controls.update();
 
 const animate = function () {
   requestAnimationFrame(animate);
-  cube1.rotation.x += 0.01;
-  cube1.rotation.y += 0.01;
+  controls.update();
+
+  // mondrian.getObjectByName("huge-white").rotation.x += 0.01;
+  // mondrian.getObjectByName("huge-white").rotation.y += 0.01;
 
   renderer.render(scene, camera);
 };
 
 animate();
+new THREE.Box3().setFromObject(mondrian).getCenter(mondrian.position).multiplyScalar(-1);
