fcebd57f3ecf86a9df6f7a1bcd3b7d39dec46a3d
Aymeric Mortemousque <aymeric@mortemousque.com>
2020-10-09 21:06:40 +0200

    Add favicon and fix css

--- 2d/components/mondrian-piece.js
+++ 2d/components/mondrian-piece.js
@@ -4,22 +4,5 @@ export class MondrianPiece extends HTMLLIElement {
     this.gradient = false;
   }
 
-  connectedCallback() {
-    var settings = document.querySelector("lab-settings");
-    settings.addEventListener("settingsChange", this.handleSettingChange.bind(this));
-  }
-
-  handleSettingChange({ detail }) {
-    this.gradient = detail.gradient;
-    this.gradientAnimated = detail.gradientAnimated;
-    this.gradientFollowPointer = detail.gradientFollowPointer;
-
-    if (this.gradient)
-      this.style.backgroundImage =
-        "linear-gradient(deg, var(--background-darker),  var(--background-darker),  var(--background), var(--background), var(--background), var(--background)";
-    else this.style.backgroundImage = null;
-
-    if (this.gradientAnimated) this.style.animation = "gradientBG 5s ease infinite";
-    else this.style.animation = null;
-  }
+  connectedCallback() {}
 }
